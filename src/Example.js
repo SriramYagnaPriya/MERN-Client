3f469a75b62cf56f159e8b757e45148ed84d75a9
SriramYagnaPriya <sriramyagnapriya2005@gmail.com>
2024-07-08 10:08:43 +0530

    first commit

---
+++ src/Example.js
@@ -0,0 +1,17 @@
+import {Mycontext} from "./Mycontext";
+import Mycomponent from "./Mycomponent";
+import { useState } from "react";
+//provider->which produces the data/props
+export default function Example(){
+    //provide
+    const [text,setText]=useState("")
+    return(
+        <div>
+            <Mycontext.Provider value={{text,setText}}>
+                <Mycomponent/>
+                {/* you can add as many components u want provider will provide
+                all the props to the given compinents*/}
+            </Mycontext.Provider>
+        </div>
+    )
+}
